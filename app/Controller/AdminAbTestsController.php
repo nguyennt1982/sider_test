df440e68d4138ea8521b63cc0d253d6865ec9d28
nguyennt2 <nguyennt2@rikkeisoft.com>
2021-06-30 10:26:17 +0700

    add php files

---
+++ app/Controller/AdminAbTestsController.php
@@ -0,0 +1,49 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+ */
+class AdminAbTestsController extends AppController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeFilter()
+    {
+    }
+
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * add method
+     */
+    public function add()
+    {
+    }
+
+    /**
+     * edit method
+     *
+     * @param int $id AbTestId
+     */
+    public function edit($id = null)
+    {
+    }
+
+    /**
+     * delete method
+     *
+     * @param int $id AbTestId
+     * @return \Cake\Network\Response|null
+     */
+    public function delete($id = null)
+    {
+    }
+}
